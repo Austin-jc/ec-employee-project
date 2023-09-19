6b60fb8fd6b9aec2563ddf9cf72d36aee1462919
Austin Con <eh.jay.cee@gmail.com>
2023-09-19 12:34:29 -0700

    First Commit - Set up prisma and seed script

---
+++ prisma/seed.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient({});
+const fs = require("fs");
+const path = require("path");
+
+async function main() {
+  const filepath = path.resolve(__filename, "../data.json");
+  fs.readFile(filepath, "utf8", async (error: Error, data: any) => {
+    if (error) {
+      console.error("error reading JSON file: ", error);
+      return;
+    }
+    try {
+      const jsonData = JSON.parse(data);
+      console.log(jsonData);
+      try {
+        await prisma.user.createMany({
+          data: jsonData.employees,
+        });
+      } catch {
+        console.error("Problem creating records while seeding");
+      }
+    } catch (error) {
+      console.error("Error parsing JSON data:", error);
+    }
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
---
+++ src/utils/prisma.ts
@@ -0,0 +1,10 @@
+// create one instance of prisma for use within app
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
