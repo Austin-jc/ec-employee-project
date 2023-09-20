ef5d78ef02d21eb9e66f2002cd4218191b8cd17f
Austin Con <eh.jay.cee@gmail.com>
2023-09-20 11:02:44 -0700

    add dollar sign

--- src/components/Tables/EditCell.jsx
+++ src/components/Tables/EditCell.jsx
@@ -21,7 +21,9 @@ export default function EditCell({
           />
         </div>
       ) : (
-        target[propertyName]
+        <div>
+          {`${propertyName === "salary" ? "$" : ""}${target[propertyName]}`}
+        </div>
       )}
     </div>
   );
